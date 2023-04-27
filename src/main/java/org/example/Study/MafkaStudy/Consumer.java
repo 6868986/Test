c2e1535f04b0f80e9909f9a903cde28779a17eea
liushixing <liushixin@meituan.com>
2023-04-27 17:12:38 +0800

    MafkaDemo

--- src/main/java/org/example/Study/MafkaStudy/Consumer.java
+++ src/main/java/org/example/Study/MafkaStudy/Consumer.java
@@ -4,6 +4,16 @@
  */
 package org.example.Study.MafkaStudy;
 
+import com.meituan.mafka.client.MafkaClient;
+import com.meituan.mafka.client.consumer.ConsumeStatus;
+import com.meituan.mafka.client.consumer.ConsumerConstants;
+import com.meituan.mafka.client.consumer.IConsumerProcessor;
+import com.meituan.mafka.client.consumer.IMessageListener;
+import com.meituan.mafka.client.message.MafkaMessage;
+import com.meituan.mafka.client.message.MessagetContext;
+
+import java.util.Properties;
+
 /**
  * <p>
  *
@@ -13,4 +23,25 @@ package org.example.Study.MafkaStudy;
  * @date 2023/4/26 15:59
  */
 public class Consumer {
+
+    public static IConsumerProcessor consumer;
+
+    public static void main(String[] args) throws Exception {
+
+        Properties properties = new Properties();
+
+        properties.setProperty(ConsumerConstants.MafkaBGNamespace,"bgname");
+        properties.setProperty(ConsumerConstants.MafkaClientAppkey,"appkey");
+        properties.setProperty(ConsumerConstants.SubscribeGroup,"consumer_group");
+
+        consumer = MafkaClient.buildConsumerFactory(properties,"topic_name");
+        //参数1：消息class   参数2：消费逻辑封装IMessageListener
+        consumer.recvMessageWithParallel(String.class, new IMessageListener() {
+            @Override
+            public ConsumeStatus recvMessage(MafkaMessage mafkaMessage, MessagetContext messagetContext) {
+                //消费逻辑
+                return null;
+            }
+        });
+    }
 }
--- src/main/java/org/example/Study/MafkaStudy/Producer.java
+++ src/main/java/org/example/Study/MafkaStudy/Producer.java
@@ -4,6 +4,15 @@
  */
 package org.example.Study.MafkaStudy;
 
+import com.meituan.mafka.client.MafkaClient;
+import com.meituan.mafka.client.consumer.ConsumerConstants;
+import com.meituan.mafka.client.producer.AsyncProducerResult;
+import com.meituan.mafka.client.producer.FutureCallback;
+import com.meituan.mafka.client.producer.IProducerProcessor;
+import com.meituan.mafka.client.producer.ProducerResult;
+
+import java.util.Properties;
+
 /**
  * <p>
  *
@@ -13,4 +22,42 @@ package org.example.Study.MafkaStudy;
  * @date 2023/4/26 15:59
  */
 public class Producer {
+
+    private static IProducerProcessor producer;
+
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+
+        properties.setProperty(ConsumerConstants.MafkaBGNamespace,"bgname");
+        properties.setProperty(ConsumerConstants.MafkaClientAppkey,"appkey");
+
+        producer = MafkaClient.buildProduceFactory(properties,"topic_name");
+
+        for(int i = 0;i < 10;i++){
+
+            try{
+                //同步发送
+                ProducerResult producerResult = producer.sendMessage("这是第" + i + "条消息");
+
+                //异步发送
+                String message = "这是第" + i + "条消息";
+                ProducerResult producerResult1 = producer.sendAsyncMessage(message,
+                        new FutureCallback() {
+                            @Override
+                            public void onSuccess(AsyncProducerResult asyncProducerResult) {
+                                System.out.println("发送成功" + message);
+                            }
+
+                            @Override
+                            public void onFailure(Throwable throwable) {
+                                System.out.println("发送失败" + message);
+                            }
+                        }
+                );
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
 }
